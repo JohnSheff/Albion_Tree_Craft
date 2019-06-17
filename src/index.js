76f433a1f8177208bd66b78cdf9d103281543553
Evgeniy <pivo223@mail.ru>
2019-06-17 20:57:07 +0300

    смена бутстрапа

--- src/index.js
+++ src/index.js
@@ -1,54 +1,82 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import { Form } from 'bootstrap-4-react';
-import Towm from "./components/towns/town"
-import Tier from "./components/tiers/tier-resourse"
-import Resourse from "./components/resourse/resourse"
-
-import "./index.css"
-
+import { Form } from "antd";
+import Towm from "./components/towns/town";
+import Tier from "./components/tiers/tier-resourse";
+import Resourse from "./components/resourse/resourse";
+// import {Fort} from "./components/towns"
+import "./index.css";
+import 'antd/dist/antd.css';
 class App extends Component {
+  state = {
+    town: undefined,
+    res: undefined,
+    persent: 15
+  };
 
+valueSelect =(props) =>{
+this.setState({
+
+})
+  
+}
+  // funcValueTowm =(props) =>{
+  //   if(Fort.some(Element=> Element === JSON.stringify(Fort[0])) {
+
+  //   }
+  // }
   render() {
     return (
       <div className="main-page">
-        <div className="row" style={{ margin: "15px", border: "2px solid"  }}  >
-        <Towm />
-     
+        <div className="row" style={{ margin: "15px", border: "2px solid" }}>
+          <Towm />
           <Resourse />
           <Tier />
-         
         </div>
         <div className="row">
           <input
-            class="form-control" style={{ margin: 30, width: "auto" }} type="text" placeholder="Вводим количество">
-          </input>
-          <div className="col col-sm-3">
-          <input
-            class="form-control" style={{ margin: 30, width: "" }} type="text" placeholder="Результат подсчета готового ресурса" disabled>
-          </input>
-            
+            className="form-control"
+            style={{ margin: 30, width: "auto" }}
+            type="text"
+            placeholder="Вводим количество"
+          />
+          <div className="col col-sm-2  ">
+            <input
+              className="form-control"
+              style={{ margin: 30, width: "" }}
+              type="text"
+              placeholder="Результат подсчета готового ресурса"
+              disabled
+            />
           </div>
           <Form.Check>
             <Form.Checkbox id="defaultCheck1" />
-            <Form.CheckLabel htmlFor="defaultCheck1">разв дерево</Form.CheckLabel>
+            <Form.CheckLabel htmlFor="defaultCheck1">
+              разв дерево
+            </Form.CheckLabel>
           </Form.Check>
-          <div className="col col-sm-3" >
-            
-          <input
-            class="form-control" style={{ margin: 30, width: "" }} type="text" placeholder="результат подсчета обычного ресурса" disabled>
-          </input>
+          <div className="col col-sm-2">
+            <input
+              className="form-control"
+              style={{ margin: 30, width: "" }}
+              type="text"
+              placeholder="результат подсчета обычного ресурса"
+              disabled
+            />
           </div>
-          <div className="col col-sm-3" >
-                    <input
-            class="form-control" style={{ margin: 30, width: "" }} type="text" placeholder="результат после переработки % от городов" disabled>
-          </input>
+          <div className="col col-sm-2">
+            <input
+              className="form-control"
+              style={{ margin: 30, width: "" }}
+              type="text"
+              placeholder="результат после переработки % от городов"
+              disabled
+            />
           </div>
         </div>
       </div>
     );
-
   }
 }
 const rootElement = document.getElementById("root");
-ReactDOM.render(< App />, rootElement);
+ReactDOM.render(<App />, rootElement);
