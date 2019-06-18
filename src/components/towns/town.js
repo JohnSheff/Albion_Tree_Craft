df06796f8dabad6d23c8488afc5e1f3f11267c25
pivo223 <pivo223@mail.ru>
2019-06-18 20:17:47 +0300

    добавлен input

--- src/components/towns/town.js
+++ src/components/towns/town.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
-import { Select, } from "antd";
+import { Select, Input } from "antd";
+
 export const Fort = [
   { town: "Тетфорт", res: "Руда" },
   { town: "Мартлок", res: "Кожа" },
@@ -13,15 +14,18 @@ export default class Town extends Component {
   render() {
     return (
       <div style={{ margin: "10px", width: "300px" }}>
+         <Input.Group >
+       
         <Select style={{ margin: "10px", width: "300px" }} defaultValue={"Выбор города"} onChange={(e) => { console.log(e) }}        >
-          {/* <Input.Group key={}></Input.Group> */}
+        
           <Select.Option value={Fort[0].town}>{Fort[0].town}</Select.Option>
           <Select.Option value={Fort[1].town}>{Fort[1].town}</Select.Option>
           <Select.Option value={Fort[2].town}>{Fort[2].town}</Select.Option>
           <Select.Option value={Fort[3].town}>{Fort[3].town}</Select.Option>
           <Select.Option value={Fort[4].town}>{Fort[4].town}</Select.Option>
         </Select>
-
+        <Input placeholder="15%" disabled type="text" style={{ width: "55px" }}></Input>
+        </Input.Group>
       </div>
     );
   }
