ee00383fc55418660d3e61f5442a874a85c7c42d
pivo223 <pivo223@mail.ru>
2019-06-20 13:53:04 +0300

    q

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import { Checkbox, Row, Col, Input,Button  } from 'antd';
+import { Checkbox, Row, Col, Input, Button } from 'antd';
 import Town from "./components/towns/town";
 import Tier from "./components/tiers/tier-resourse";
 import Resourse from "./components/resourses/resourse";
@@ -12,22 +12,20 @@ import 'antd/dist/antd.css';
 
 class App extends Component {
 
- 
+
   valueSelectTown = (valTown) => {
     this.setState({ town: valTown })
-    const date= new Date()
-    console.log(date.getMilliseconds())
-     }
+  
+  }
 
   valueSelectRes = (valRes) => {
     this.setState({ res: valRes })
-    const date= new Date()
-    console.log(date.getMilliseconds())
+ 
   }
 
   valueSelectTier = (valTier) => {
     this.setState({ tier: valTier })
-    
+
   }
 
   valuePersent = () => {
@@ -47,62 +45,61 @@ class App extends Component {
 
   };
   render() {
-const{town, persent,res, tier} = this.state
-const date= new Date()
-console.log(date.getMilliseconds())
+    const { town, persent, res, tier } = this.state
+   
     return (
       <div className="main-page">
-        <Row style={{ border: "2px solid", padding: 10 }} gutter={5}>
-          <Col span={2}>
+        <Row style={{ border: "2px solid", padding: 10 ,margin: 3}} gutter={5}>
+          <Col xs= {12} sm={12} md= {6}>
             <Town selectTown={this.valueSelectTown} valueTown={town} />
           </Col>
-          <Col span={3}>
-            <Button style={{ backgroundColor: "#ffffff", color: "red", textAlign: "center", width: "auto" }}
-            onClick={() => {this.valuePersent()}}>{"Посчитать % возврата " +  persent }</Button >
-          </Col>
-          <Col span={2}>
+          <Col xs= {12} sm={12} md= {6}>
             <Resourse selectRes={this.valueSelectRes} valueRes={res} />
           </Col>
-          <Col span={3}>
+          <Col xs= {12} sm={12} md= {6}>
+            <Button style={{ backgroundColor: "#ffffff", color: "red", textAlign: "center", width: "auto" }}
+              onClick={() => { this.valuePersent() }}>{"Посчитать % возврата " + persent}</Button >
+          </Col>
+          <Col xs= {12} sm={12} md= {6}>
             <Tier selectTier={this.valueSelectTier} valueTier={tier} />
           </Col>
         </Row>
-        <Row>
-          <Col span={4}>
+        <Row style={{ border: "2px solid", padding: 10,margin: 3 }} gutter={5}>
+          <Col xs= {6} sm={6} md= {3}>
             <Input
               className="form-control"
-              style={{ margin: 30, width: "280px" }}
+              style={{ margin: 30, width: "auto" }}
               type="text"
               placeholder="Вводим количество"
             />
           </Col>
-          <Col span={5}>
+          <Col xs= {6} sm={6} md= {3}>
             <Input
               className="form-control"
-              style={{ margin: 30, width: "280px" }}
+              style={{ margin: 30, width: "auto" }}
               type="text"
               placeholder="Результат подсчета готового ресурса"
               disabled
             />
           </Col>
-          <Col span={2}>
+          <Col xs= {3} sm={3} md= {2}>
             <Checkbox.Group style={{ width: 'auto' }}>
               <Checkbox value="Х">Раскрыть дерево</Checkbox>
             </Checkbox.Group>
           </Col>
-          <Col span={5}>
+          <Col xs= {6} sm={6} md= {3}>
             <Input
               className="form-control"
-              style={{ margin: 30, width: "280px" }}
+              style={{ margin: 30, width: "auto" }}
               type="text"
               placeholder="результат подсчета обычного ресурса"
               disabled
             />
           </Col>
-          <Col span={5}>
+          <Col xs= {6} sm={6} md= {3}>
             <Input
               className="form-control"
-              style={{ margin: 30, width: "300px" }}
+              style={{ margin: 30, width: "auto" }}
               type="text"
               placeholder="результат после переработки % от городов"
               disabled
