cfb0c90b371f0f8f02a6d1ef6a7217c09dd3a6c6
Evgeniy <pivo223@mail.ru>
2019-06-22 21:32:59 +0300

    last etap

--- src/index.js
+++ src/index.js
@@ -1,61 +1,109 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import { Row, Col, Input } from 'antd';
-import InputValue from "./components/Inputs/input-value"
+import { Row, Col, Input } from "antd";
+import InputValue from "./components/Inputs/input-value";
 import Town from "./components/towns/town";
 import Tier from "./components/tiers/tier-resourse";
 import Resourse from "./components/resourses/resourse";
-import { Fort } from "./components/towns/town"
+import { Fort } from "./components/towns/town";
 import "./index.css";
-import 'antd/dist/antd.css';
-
-
+import "antd/dist/antd.css";
 
 class App extends Component {
-
-
-  valueSelectTown = (valTown) => {
-    this.setState({ town: valTown })
-    this.valuePersent(valTown, undefined)
-  }
-
-  valueSelectRes = (valRes) => {
-    this.setState({ res: valRes })
-    this.valuePersent(undefined, valRes)
-  }
-
-  valueSelectTier = (valTier) => {
-    this.setState({ tier: valTier })
-
-  }
-
-  valuePersent = (town, res) => {
-    if (Fort.some(element => JSON.stringify(element) === JSON.stringify({ town: town || this.state.town, res: res || this.state.res }))) {
-      this.setState({ persent: 35 })
-    }
-    else {
-      this.setState({ persent: 15 })
-    }
-  }
-
   state = {
     town: undefined,
     res: undefined,
     persent: 15,
-    tier: undefined
-
+    tier: undefined,
+    inputArr: [1],
+    startValue: 0
   };
+
+  oneMoreArr = val => {
+    if (this.state.inputArr.length < this.state.tier - 1) {
+      if (val) {
+        this.state.inputArr.push(true);
+        this.setState({ inputArr: this.state.inputArr });
+      } else if (this.state.inputArr.length === 1) {
+        val = false;
+      } else {
+        this.setState({
+          inputArr: this.state.inputArr.splice(
+            this.state.inputArr.length - 1,
+            1
+          )
+        });
+      }
+    }
+  };
+
+  firstValue = oneVal => {
+    this.setState({ startValue: oneVal });
+  };
+  valueSelectTown = valTown => {
+    this.setState({ town: valTown });
+    this.valuePersent(valTown, undefined);
+  };
+
+  valueSelectRes = valRes => {
+    this.setState({ res: valRes });
+    this.valuePersent(undefined, valRes);
+  };
+
+  valueSelectTier = valTier => {
+    this.setState({ tier: valTier });
+    if (valTier < this.state.inputArr.length) {
+      this.setState({
+        inputArr: this.state.inputArr.splice(
+          0,
+          this.state.inputArr.length -
+            (this.state.inputArr.length - valTier + 1)
+        )
+      });
+    }
+  };
+
+  valuePersent = (town, res) => {
+    if (
+      Fort.some(
+        element =>
+          JSON.stringify(element) ===
+          JSON.stringify({
+            town: town || this.state.town,
+            res: res || this.state.res
+          })
+      )
+    ) {
+      this.setState({ persent: 35 });
+    } else {
+      this.setState({ persent: 15 });
+    }
+  };
+
   render() {
-    const { town, persent, res, tier } = this.state
+    const { town, persent, res, tier, inputArr, startValue } = this.state;
+    console.log(this.state);
 
     return (
       <div className="main-page">
-        <Row style={{ border: "2px solid", padding: "10px", margin: 3 }} gutter={5}>
+        <Row
+          style={{ border: "2px solid", padding: "10px", margin: 3 }}
+          gutter={5}
+        >
           <Col xs={12} sm={12} md={6}>
             <Town selectTown={this.valueSelectTown} valueTown={town} />
           </Col>
           <Col xs={8} sm={4} md={2}>
-            <Input placeholder="" disabled style={{ backgroundColor: "#ffffff", color: "red", textAlign: "center" }} value={persent + " %"} />
+            <Input
+              placeholder=""
+              disabled
+              style={{
+                backgroundColor: "#ffffff",
+                color: "red",
+                textAlign: "center"
+              }}
+              value={persent + " %"}
+            />
           </Col>
           <Col xs={12} sm={12} md={6}>
             <Resourse selectRes={this.valueSelectRes} valueRes={res} />
@@ -65,7 +113,18 @@ class App extends Component {
             <Tier selectTier={this.valueSelectTier} valueTier={tier} />
           </Col>
         </Row>
-        <InputValue tierValue={tier} persentValue={persent} />
+        {inputArr.map((val, index) => {
+          return (
+            <InputValue
+              key={index}
+              tierValue={tier - index}
+              persentValue={persent}
+              inputVal={startValue}
+              fv={this.firstValue}
+              oneMoreArr={this.oneMoreArr}
+            />
+          );
+        })}
       </div>
     );
   }
